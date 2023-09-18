d49cb9a2d0e179402ab14a91122d7e17486ef62f
mudiagaobriki <mudi_invents@yahoo.com>
2023-09-18 13:25:34 +0100

    First commit 2

---
+++ src/app/about/how-treepz-works/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from "next/link";
+
+const Page = () => {
+    return (
+        <div>
+            <h1>How Treepz Works</h1>
+            <Link href='/home'>Home</Link>
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <h1>Welcome to the Home Page</h1>
+        </>
+    );
+};
+
+export default Page;
